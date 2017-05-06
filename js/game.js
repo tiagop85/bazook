092373f1afa5f648907f3b52d173471a78c14615
Claudio Sampaio <claudio.sampaio@conatussw.com>
2017-05-06 16:32:41 -0400

    Estrutura do projeto inicial

---
+++ js/game.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+var GameState = function(game) {};
+
+GameState.prototype.preload = function() {
+}
+
+GameState.prototype.create = function() {
+}
+
+GameState.prototype.update = function() {
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-canvas');
+
+game.state.add('game', GameState);
+game.state.start('game');
